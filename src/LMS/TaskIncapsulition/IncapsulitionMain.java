7bb42bcf8e79dad24fa4e685c03f4c8f3b24b75b
Elnura17 <elnuraarapova48@gmail.com>
2025-03-10 05:09:19 +0600

    Initial commit

---
+++ src/LMS/TaskIncapsulition/IncapsulitionMain.java
@@ -0,0 +1,59 @@
+package LMS.TaskIncapsulition;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class IncapsulitionMain {
+    public static void main(String[] args) {
+
+        University[] universities;
+        universities = new University[0];
+        University university = new University();
+        university.setName("КГМА");
+        university.setFaculty("Стоматолог");
+        university.setContract(57000);
+
+        University university1;
+        university1 = new University();
+        university1.setName("КГУ");
+        university1.setFaculty("Физ - мат");
+        university1.setContract(47000);
+
+
+        universities = university.createUniversity(universities, university);
+        universities = university1.createUniversity(universities, university1);
+        System.out.println(Arrays.toString(universities));
+
+        School[] schools = new School[0];
+        School school = new School();
+        school.setName("Элчибек Эркебаев");
+        school.setLanguage("кыргыз");
+        school.setNumberOfStudents(365);
+        school.setNumberOfTeachers(18);
+
+        schools = school.createSchool(schools, school);
+        System.out.println(Arrays.toString(schools));
+
+        Person[] persons = new Person[0];
+        Person person = new Person();
+        person.setFirstName("Elnura");
+        person.setLastName("Arapova");
+        person.setAge(20);
+        person.setGender('F');
+
+        persons = person.createPerson(persons, person);
+        System.out.println(Arrays.toString(persons));
+
+        Car[] cars = new Car[0];
+        Car car = new Car();
+        car.setMarca("Nexia");
+        car.setLight("Black");
+        car.setPrice(10000);
+        car.setBox("Mechanic");
+
+        cars = car.createCar(cars, car);
+        System.out.println(Arrays.toString(cars));
+
+
+    }
+}
---
+++ src/LMS/TaskIncapsulition/School.java
@@ -0,0 +1,59 @@
+package LMS.TaskIncapsulition;
+
+public class School {
+    private String name;
+    private String language;
+    private int numberOfStudents;
+    private int numberOfTeachers;
+
+    public String name() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String language() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int numberOfStudents() {
+        return numberOfStudents;
+    }
+
+    public void setNumberOfStudents(int numberOfStudents) {
+        this.numberOfStudents = numberOfStudents;
+    }
+
+    public int numberOfTeachers() {
+        return numberOfTeachers;
+    }
+
+    public void setNumberOfTeachers(int numberOfTeachers) {
+        this.numberOfTeachers = numberOfTeachers;
+    }
+
+    public School[] createSchool (School[] schools, School school){
+        School[] newSchool = new School[schools.length + 1];
+
+        for (int i = 0; i < schools.length; i++) {
+            newSchool[i] = schools[i];
+        }
+        newSchool[schools.length] = school;
+        return newSchool;
+    }
+
+    @Override
+    public String toString() {
+        return "\nSchool" +
+                "\nname = " + name +
+                "\nlanguage = " + language +
+                "\nnumber Of Students = " + numberOfStudents +
+                "\nnumber Of Teachers = " + numberOfTeachers + "\n------------";
+    }
+}
